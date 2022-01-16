10f26f74b3a37e6f0b97fc41b54a6a0f95f5a6e9
artio <artemyermolenko14@gmail.com>
2022-01-16 14:49:32 +0100

    first Commit

---
+++ Main.py
@@ -0,0 +1,38 @@
+import pygame
+import time
+
+class Main:
+    def __init__(self, width, height):
+        pygame.init()
+        bg = (124, 193, 121)
+
+        screen = pygame.display.set_mode((width, height))
+
+        pygame.display.set_caption('Snake Game')
+
+        font = pygame.font.Font('freesansbold.ttf', 32)
+        text = font.render('Score:', True, (255, 255, 255), bg)
+
+        textRect = text.get_rect()
+
+        pygame.display.flip()
+
+        screen.fill(bg)
+
+        gameRect = pygame.Rect(width * 0.07, height * 0.07, width * 0.87, height * 0.87)
+
+        pygame.draw.rect(screen, (11, 140, 32), gameRect)
+
+        while True:
+
+            screen.blit(text, textRect)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    main = Main(800, 600)
